3521adfcf56d4c8e2dcf265ec72b18deaada421e
paskoivi <pasi.koivisto@nokia.com>
2017-08-29 13:07:04 +0300

    New reader3

--- utils/src/main/java/com/nokia/oss/cm/csv/CSVReaderImpl.java
+++ utils/src/main/java/com/nokia/oss/cm/csv/CSVReaderImpl.java
@@ -8,6 +8,10 @@ import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import java.io.InputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import org.springframework.core.io.ClassPathResource;
 
 import org.springframework.stereotype.Component;
 
